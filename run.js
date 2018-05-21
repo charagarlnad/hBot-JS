7594db6c47537077fbb7959313e6aaad99374d15
charagarlnad <27380203+charagarlnad@users.noreply.github.com>
2018-05-21 13:32:40 -0400

    eslint lmfoa

--- run.js
+++ run.js
@@ -2,20 +2,20 @@ const Eris = require('eris');
 const CONFIG = require('./config.json');
 
 var bot = new Eris.CommandClient(CONFIG.token, {}, {
-  description: 'A epic bot that is slightly less shit now.',
-  owner: 'Chara#0001',
-  prefix: CONFIG.prefix
+    description: 'A epic bot that is slightly less shit now.',
+    owner: 'Chara#0001',
+    prefix: CONFIG.prefix
 });
 
 console.log('fuck');
 
 bot.registerCommand('h', (msg) => {
-  console.log('fuckass');
-  msg.channel.createMessage('h');
+    console.log('fuckass');
+    msg.channel.createMessage('h');
 }, {
-  description: "h",
-  fullDescription: "hhhhhhhhh",
-  usage: "no"
+    description: 'h',
+    fullDescription: 'hhhhhhhhh',
+    usage: 'no'
 });
 
 bot.connect();
