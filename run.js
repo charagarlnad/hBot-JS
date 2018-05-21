a1a47a5a0435dca02dfed2bc4f36904b9e1e7c49
charagarlnad <27380203+charagarlnad@users.noreply.github.com>
2018-05-21 13:19:44 -0400

    please excuse my dope ass swag

---
+++ run.js
@@ -0,0 +1,21 @@
+const Eris = require('eris');
+const CONFIG = require('./config.json');
+
+var bot = new Eris.CommandClient(CONFIG.token, {}, {
+  description: 'A epic bot that is slightly less shit now.',
+  owner: 'Chara#0001',
+  prefix: CONFIG.prefix
+});
+
+console.log('fuck');
+
+bot.registerCommand('h', (msg) => {
+  console.log('fuckass');
+  msg.channel.createMessage('h');
+}, {
+  description: "h",
+  fullDescription: "hhhhhhhhh",
+  usage: "no"
+});
+
+bot.connect();
